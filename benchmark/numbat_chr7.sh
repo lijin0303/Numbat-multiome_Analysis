77ea6c1acc105e8ac0d8d824a8bd1d36e8c83efc
Ruitong Li <ruitong@broadinstitute.org>
2025-07-12 19:44:52 -0400

    run_numbat

--- benchmark/numbat_chr7.sh
+++ benchmark/numbat_chr7.sh
@@ -80,10 +80,10 @@ gtfF="hg38"                # or path/to/gtf_hg38.gtf
 
 
 # parL="par_numbatm.rds" # a list of any run_numbat parameters you would like to optimize
-# Rscript benchmark/run_numbat_multiome.R  \
-# 			--countmat benchmark/benchmark/${prefix}_comb_bincnt.rds \
-# 			--alleledf ${sample}/${sample}_comb_allele_counts.tsv.gz \
-# 			--out_dir benchmark/${sample}_${prefix}/ \
-# 			--ref  benchmark/lambdas_comb_bincnt_${prefix}.rds \
-# 			--gtf  benchmark/${binGR}\
-# 			--parL ${parL}
+Rscript benchmark/run_numbat_multiome.R  \
+        --countmat benchmark/benchmark/${prefix}_comb_bincnt.rds \
+        --alleledf benchmark/${sample}_allele_counts_chr7.tsv.gz \
+        --out_dir benchmark/${sample}_${prefix}/ \
+        --ref  benchmark/lambdas_comb_bincnt_${prefix}.rds \
+        --gtf  benchmark/${binGR}\
+        --parL ${parL}
