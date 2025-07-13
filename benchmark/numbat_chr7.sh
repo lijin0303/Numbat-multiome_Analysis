4e3c74d1ccbee363f97450ed60ef54ef6ded8756
Ruitong Li <ruitong@broadinstitute.org>
2025-07-12 20:46:24 -0400

    modify input

--- benchmark/numbat_chr7.sh
+++ benchmark/numbat_chr7.sh
@@ -14,11 +14,14 @@ sample="patA"
 inputdir="${workdir}/${sample}_${prefix}_inputs"
 outdir="${workdir}/${sample}_${prefix}_outputs"
 mkdir -p "$inputdir" "$outdir"
-
+# 2. Move binGR if present in workdir
+if [ -f "${workdir}/${binGR}" ]; then
+    mv "${workdir}/${binGR}" "${inputdir}/${binGR}"
+fi
 # 3. Gene→bin map (prefix-specific → $inputdir)
 Rscript "${rsrc}/get_gene_binned_intersections.R" \
   --numbatGTFname "${gtfF}" \
-  --binGR          "${rsrc}/${binGR}" \
+  --binGR          "${inputdir}/${binGR}" \
   --outfile        "${inputdir}/${prefix}_gene2bin_map.csv"
 
 # 4. Binned inputs for your sample
@@ -33,7 +36,7 @@ Rscript "${rsrc}/get_binned_rna.R" \
 Rscript "${rsrc}/get_binned_atac.R" \
   --CB      "${workdir}/test_ATAC.txt" \
   --frag    "${workdir}/chr7.fragments.tsv.gz" \
-  --binGR   "${rsrc}/${binGR}" \
+  --binGR   "${inputdir}/${binGR}" \
   --outFile "${inputdir}/${prefix}_atac_bin.rds"
 
 ## 4c. Combine RNA & ATAC → $inputdir
@@ -66,7 +69,7 @@ Rscript "${rsrc}/get_binned_rna.R" \
 Rscript "${rsrc}/get_binned_atac.R" \
   --frag    "${workdir}/chr7.fragments.tsv.gz" \
   --CB      "${workdir}/normal_ATAC_annot.tsv" \
-  --binGR   "${rsrc}/${binGR}" \
+  --binGR   "${inputdir}/${binGR}" \
   --outFile "${inputdir}/lambdas_ATAC_bincnt_${prefix}.rds" \
   --generateAggRef
 
@@ -93,7 +96,7 @@ Rscript "${rsrc}/run_numbat_multiome.R" \
   --alleledf "${workdir}/${sample}_allele_counts_chr7.tsv.gz" \
   --out_dir  "${outdir}" \
   --ref      "${inputdir}/lambdas_comb_bincnt_${prefix}.rds" \
-  --gtf      "${rsrc}/${binGR}" \
+  --gtf      "${inputdir}/${binGR}" \
   --parL     "${workdir}/par_comb_bincnt.rds"
 
 echo "Done!
