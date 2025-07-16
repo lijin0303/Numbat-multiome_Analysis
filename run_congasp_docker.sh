2f93abe1eac76a2476fd32482da137023869f362
Ruitong Li <ruitong@broadinstitute.org>
2025-07-16 16:50:20 -0400

    docker run update

--- run_congasp_docker.sh
+++ run_congasp_docker.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # Usage: bash run_congasp_docker.sh
 # This script launches the Docker container, mounts $PWD/congasp as /workspace, and runs run_congasp_training.sh inside the container.
+# 
+# Before running, replace 'yourusername' with your actual Docker Hub username:
+# DOCKER_IMAGE="yourusername/congas:latest"
 
-DOCKER_IMAGE="congas:latest"
+DOCKER_IMAGE="ruitongl/congasp:latest"
 
 # Run the script inside the container with increased memory limits
 docker run --rm \
